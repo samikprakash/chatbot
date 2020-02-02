6c57fee15043f67660583aca1f5bb158ed538814
samikprakash <samikprakash@gmail.com>
2020-02-02 13:50:39 +0530

    first commit

--- train.py
+++ train.py
@@ -18,53 +18,44 @@ intents = json.loads(data_file)
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        #tokenize each word
+
         w = nltk.word_tokenize(pattern)
-        # print(w,"W")
+
         words.extend(w)
-        #add documents in the corpus
+
         documents.append((w, intent['tag']))
 
-        # add to our classes list
+
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# lemmaztize and lower each word and remove duplicates
+
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-# sort classes
-# classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-# print (len(documents), "documents",documents)
-# classes = intents
-# print (len(classes), "classes", classes)
-# words = all words, vocabulary
-# print (len(words), "unique lemmatized words", words)
+
 
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
 training = []
-# create an empty array for our output
+
 output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
+
 for doc in documents:
-    # initialize our bag of words
+
     bag = []
-    # list of tokenized words for the pattern
+
     pattern_words = doc[0]
-    # print("pattern words ",pattern_words)
-    # lemmatize each word - create base word, in attempt to represent related words
+  
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+  
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    # print("bag",bag)
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
+
     output_row = list(output_empty)
-    # print("output_wor before",output_row)
+   
     output_row[classes.index(doc[1])] = 1
-    # print("output_wor after",output_row)
+  
     training.append([bag, output_row])
 
 random.shuffle(training)
